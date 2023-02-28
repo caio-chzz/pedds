8232856b66b108055edc450558f8855d6ab5021e
caio fabio gregio <111304209+caio-chzz@users.noreply.github.com>
2023-02-28 09:06:22 -0300

    Add files via upload
    aula 3

---
+++ Lista1/src/exercicico/Exercicio.java
@@ -0,0 +1,19 @@
+package exercicico;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("insira preco");
+		JOptionPane.showInputDialog("insira quantidade de litros");
+		
+		public class calculapreco{
+			
+			
+		JOptionPane.showMessageDialog(null, "total a paagar");
+		
+	}
+
+}
+}
